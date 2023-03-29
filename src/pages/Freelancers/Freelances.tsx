930cf1476e3268c45d4be43c76e47d1a62e679de
QuentinMotte <99801733+QuentinMotte@users.noreply.github.com>
2023-03-29 14:48:51 +0200

    add api call profile + questions + error mngmt + loading mngmt

--- src/pages/Freelancers/Freelances.tsx
+++ src/pages/Freelancers/Freelances.tsx
@@ -1,35 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Freelances.scss'
 import { Card } from '../../components/Card'
+import axios from 'axios'
 
-const freelancerProfiles = [
-  {
-    name: 'Jane Doe',
-    jobTitle: 'Devops',
-  },
-  {
-    name: 'John Doe',
-    jobTitle: 'Frontend developer',
-  },
-  {
-    name: 'Jean Bug',
-    jobTitle: 'Fullstack Developer',
-  },
-]
+interface Freelancer {
+  name: string
+  job: string
+  picture: string
+}
 
 const Freelances = () => {
+  const [freelancers, setFreelancers] = useState<Freelancer[]>([])
+  const [isDataLoading, setDataLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+
+  const fetchFreelancers = async () => {
+    setDataLoading(true)
+    try {
+      const response = await axios.get('http://localhost:8000/freelancers')
+      setFreelancers(response.data.freelancersList)
+      setDataLoading(false)
+    } catch (err: any) {
+      setError(err.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchFreelancers()
+  }, [])
+
   return (
     <div className="freelances-container">
-      <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
-      <div className="cards-container">
-        {freelancerProfiles.map((profile, index) => (
-          <Card
-            key={`${profile.name}-${index}`}
-            label={profile.jobTitle}
-            title={profile.name}
-          />
-        ))}
-      </div>
+      <h1>Find your service provider</h1>
+      <h2>Here at Shiny we bring together the best profiles for you.</h2>
+      {error && <div className="error">{error}</div>}
+      {isDataLoading ? (
+        <div className="loader-wrapper">
+          <div className="loader"></div>
+        </div>
+      ) : (
+        <div className="cards-container">
+          {freelancers.map((profile, index) => (
+            <Card
+              key={`${profile.name}-${index}`}
+              label={profile.job}
+              title={profile.name}
+              picture={profile.picture}
+            />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
--- src/pages/Survey/Survey.tsx
+++ src/pages/Survey/Survey.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import './Survey.scss'
 
-import { useParams } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import axios from 'axios'
+
+interface SurveyData {
+  [key: number]: string
+}
 
 const Survey = () => {
   const { questionNumber } = useParams()
@@ -11,12 +14,16 @@ const Survey = () => {
   const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
   const nextQuestionNumber = questionNumberInt + 1
 
+  const [surveyData, setSurveyData] = useState<SurveyData>({})
+  const [isDataLoading, setDataLoading] = useState(false)
   const [error, setError] = useState<string | null>(null)
 
   const fetchSurveyData = async () => {
+    setDataLoading(true)
     try {
       const response = await axios.get('http://localhost:8000/survey')
-      console.log(response.data.surveyData)
+      setSurveyData(response.data.surveyData)
+      setDataLoading(false)
     } catch (err: any) {
       setError(err.message)
     }
@@ -28,15 +35,21 @@ const Survey = () => {
 
   return (
     <div className="survey-container">
-      <div>{error && <div className="error">{error}</div>}</div>
-      <h1>Survey 🧮</h1>
+      {error && <div className="error">{error}</div>}
       <h2>Question {questionNumber}</h2>
-      <Link to={`/survey/${prevQuestionNumber}`}>Back</Link>
-      {questionNumberInt === 10 ? (
-        <Link to="/results">Results</Link>
+      {isDataLoading ? (
+        <div className="loader"></div>
       ) : (
-        <Link to={`/survey/${nextQuestionNumber}`}>Next</Link>
+        <span>{surveyData[questionNumberInt]}</span>
       )}
+      <div className="link-wrapper">
+        <Link to={`/survey/${prevQuestionNumber}`}>Back</Link>
+        {surveyData[questionNumberInt + 1] ? (
+          <Link to={`/survey/${nextQuestionNumber}`}>Next</Link>
+        ) : (
+          <Link to="/results">Results</Link>
+        )}
+      </div>
     </div>
   )
 }
